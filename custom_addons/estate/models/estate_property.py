631e33f1231440285f490ca188f44a72af51ce2b
aahma <aahma@LAPTOP-HVKUOEJO>
2025-06-24 15:47:30 +0330

    Test commit

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -10,7 +10,6 @@ class EstateProperty(models.Model):
     sale_person_id = fields.Many2one('res.users', string='Sale Person',default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", string="Offers")
-
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
