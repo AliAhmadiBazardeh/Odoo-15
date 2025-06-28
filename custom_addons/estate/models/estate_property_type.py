16402875d3aef30ac844ef4fce1501017472238b
aahma <aahma@LAPTOP-HVKUOEJO>
2025-06-28 16:41:33 +0330

    Add statusbar widget for state in estate_property.py

--- custom_addons/estate/models/estate_property_type.py
+++ custom_addons/estate/models/estate_property_type.py
@@ -5,6 +5,7 @@ class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id", string="Estate Properties")
 
     _sql_constraints = [
         ('unique_property_type', 'UNIQUE(name)', 'The estate property type must be unique.'),
