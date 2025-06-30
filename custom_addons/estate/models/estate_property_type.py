c8dcc85b0df9dd34e67e9b6c74caedc58c8e158b
aahma <aahma@LAPTOP-HVKUOEJO>
2025-06-30 09:36:43 +0330

    Add sequence field to estate_property_type.py for manual ordering

--- custom_addons/estate/models/estate_property_type.py
+++ custom_addons/estate/models/estate_property_type.py
@@ -3,11 +3,11 @@ from odoo import models, fields
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
-    _order = "name"
+    _order = "sequence,name"
 
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Estate Properties")
-
+    sequence =fields.Integer('Sequence',default=1)
     _sql_constraints = [
         ('unique_property_type', 'UNIQUE(name)', 'The estate property type must be unique.'),
     ]
