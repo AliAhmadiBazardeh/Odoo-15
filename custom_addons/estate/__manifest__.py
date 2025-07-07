f37afa439e80a4af750c556e737a67eec9f1180e
aahma <aahma@LAPTOP-HVKUOEJO>
2025-07-07 13:13:50 +0330

    Add group user

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -1,16 +1,18 @@
 {
     'name': 'Estate',
-    'sequence':-1,
-    'application':True,
+    'sequence': -1,
+    'category': 'Real_State/State',
+    'application': True,
     'data': [
-    'security/ir.model.access.csv',
-    'views/estate_property_views.xml',
-    'views/estate_user_inherit_views.xml',
-    'views/estate_property_type_views.xml',
-    'views/estate_property_offer_views.xml',
-    'views/estate_property_tag_views.xml',
-    'views/estate_property_feature_views.xml',
-    'views/estate_menus.xml'
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_user_inherit_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_offer_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_feature_views.xml',
+        'views/estate_menus.xml'
     ],
     'icon': 'estate/static/description/icon.png',
 }
